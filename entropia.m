a3183399ed8fa3ceb6faf9b6a85754226f09482e
Mariana Cruz <65827628+marianafcruz17@users.noreply.github.com>
2022-04-02 19:30:23 -0300

    Update entropia.m

--- entropia.m
+++ entropia.m
@@ -1,27 +1,8 @@
-function H = entropia(x)
-    if (ischar(s)==1)
-        l = length(s);
-        caractere_unico = unique(s);
-        lenChar = length(caractere_unico);
-
-        f = zeros(1,lenChar);
-
-        for i=1:lenChar
-            f(i) = length(encontrar_p(p,caractere_unico(i)));  
+function H = entropia(p)
+    H = 0
+    for i = 1:length(p) 
+        if p(i) ~= 0
+            H = H + (-p(i)*log2(p(i)));
         end
-
-        probabilidade = zeros(1,lenChar);
-        
-        for i=1:lenChar
-            probabilidade(i) = f(i)/l;      
-        end
-
-        H = 0;
-
-        for  i=1:lenChar
-            H = H + (-probabilidade(i)*log2(probabilidade(i)));  
-        end 
-        else
-            display('Invalid String');
     end
 end
